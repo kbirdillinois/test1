55abca273302c282a7274caeb64039a352771df0
Amy <ahovious>
2018-01-09 11:58:40 -0600

    Add mole to atoms converter function

--- conversion.py
+++ conversion.py
@@ -1,1 +1,13 @@
 dfdsfdsf
+# My Conversion Tools
+
+def dollars2cents(dollars):
+    cents = dollars * 100
+    return cents
+
+...
+
+def mole2atoms(mol):
+    atoms = mole * 6.02e23
+    return atoms
+
